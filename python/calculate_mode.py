1b84ef04f5d86e36ef58d73c1d1f38f2ce578965
stesun2 <ss27@illinois.edu>
2021-05-31 19:07:37 -0500

    created basic unittest

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,6 @@
-def calculate_mode():
-    pass
+def calculate_mode(arr):
+
+    return arr
+    # pass
+
+print(calculate_mode([[1,2,3,4]]))
--- python/calculate_mode_spec.py
+++ python/calculate_mode_spec.py
@@ -1,1 +1,32 @@
 # Write your own unit tests here!
+import unittest
+from calculate_mode import calculate_mode
+
+class CalculateModeTestCase(unittest.TestCase):
+    """Test for calculate_mode.py"""
+
+    def test_returns_empty_list(self):
+        """Calls calculate_mode.py, returns an empty list"""
+        self.assertEqual(len(calculate_mode()), 0)
+    
+    def test_returns_length_list(self):
+        """Calls calculate_mode.py, returns the length of array """
+        self.assertEqual(len(calculate_mode([1,2,3,4])), 4)
+
+    def test_returns_mode_list(self):
+        """Calls calculate_mode.py, returns list mode"""
+        self.assertEqual(type(calculate_mode([1,2,3,4,4,4])), [3])
+
+    def test_returns_non_int_list(self):
+        """Calls calculate_mode.py, returns non-int mode"""
+        self.assertEqual(type(calculate_mode(['dog','cat','dogs','cat','cat'])), ['cat'])
+
+    def test_returns_equal_mod_list(self):
+        """Calls calculate_mode.py, returns list of equal frequency"""
+        self.assertEqual(type(calculate_mode([1,2,3,4])), [1,2,3,4])
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
